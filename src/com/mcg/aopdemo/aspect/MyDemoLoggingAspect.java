91e95a81b73921a0c638108b218c449f27abbf95
Muhammetcan Gul <muhammetcangl@gmail.com>
2019-06-01 15:13:05 +0300

    @AfterReturning annotation added.

--- src/com/mcg/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/mcg/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,15 +2,51 @@ package com.mcg.aopdemo.aspect;
 
 import com.mcg.aopdemo.Account;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Aspect
 @Component
 @Order(2)
 public class MyDemoLoggingAspect {
+
+    // add a new advice for @AfterReturning on the findAccounts method
+    @AfterReturning(pointcut = "execution(* com.mcg.aopdemo.dao.AccountDAO.findAccounts(..))", returning = "result")
+    public void afterReturningFindAccountsAdvice(JoinPoint joinPoint, List<Account> result){
+
+        // print out which method we are advising on
+        String method = joinPoint.getSignature().toShortString();
+        System.out.println("=========>>> Executing @AfterRetuning on method: " + method);
+
+        // print out the results of the method call
+        System.out.println("=========>>> result is :" + result);
+
+        //let's post-process the data ... let's modify it :-)
+
+        // convert the account names to uppercase
+        convertAccountNamesToUpperCase(result);
+
+    }
+
+    private void convertAccountNamesToUpperCase(List<Account> result) {
+
+        // loop through accounts
+
+        for(Account account: result){
+
+            // get uppercase version of name
+            String upperName = account.getName().toUpperCase();
+
+            // update the name on the account
+            account.setName(upperName);
+        }
+
+    }
 
     @Before("com.mcg.aopdemo.aspect.McgAopExpressions.forDaoPackageNoGetterSetter()")
     public void beforeAddAccountAdvice(JoinPoint joinPoint){
--- src/com/mcg/aopdemo/dao/AccountDAO.java
+++ src/com/mcg/aopdemo/dao/AccountDAO.java
@@ -3,11 +3,32 @@ package com.mcg.aopdemo.dao;
 import com.mcg.aopdemo.Account;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class AccountDAO {
 
     private String name;
     private String serviceCode;
+
+    // add a new method : findAccounts()
+    public List<Account> findAccounts(){
+
+        List<Account> accounts = new ArrayList<>();
+
+        // create sample accounts
+        Account account1 = new Account("Muhammetcan", "gl");
+        Account account2 = new Account("deneme1", "deneme1");
+        Account account3 = new Account("deneme2", "deneme2");
+
+        // add them to our accounts list
+        accounts.add(account1);
+        accounts.add(account2);
+        accounts.add(account3);
+
+        return accounts;
+    }
 
     // added parameter
     public void addAccount(Account account, boolean vipFlag){
